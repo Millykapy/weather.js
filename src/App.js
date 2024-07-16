5ea3e44d486285ed02c7cfd24c5d6dee7c3e9d97
Millykapy <168423257+Millykapy@users.noreply.github.com>
2024-07-16 16:20:17 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1> Spooky Weather </h1>
+      <div className="container">
+        <h1> Spooky Weather </h1>
 
-      <footer>
-        This project was created by
-        <a href="https://www.shecodes.io/graduates/120875-millicent-kapinga">
-          Millicent Kapinga
-        </a>{" "}
-        and is{" "}
-        <a href="https://github.com/Millykapy/weather.js ">
-          Open-sourced on Github
-        </a>
-      </footer>
+        <footer>
+          This project was created by{" "}
+          <a href="https://www.shecodes.io/graduates/120875-millicent-kapinga">
+            Millicent Kapinga
+          </a>{" "}
+          and is{" "}
+          <a href="https://github.com/Millykapy/weather.js ">
+            Open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
