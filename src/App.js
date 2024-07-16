e50e1bcea87bfdf39d4f82e51f510da415f324d4
Millykapy <168423257+Millykapy@users.noreply.github.com>
2024-07-16 14:53:51 +0200

    added Github url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App"> Spooky Weather </div>;
+  return (
+    <div className="App">
+      <h1> Spooky Weather </h1>
+
+      <footer>
+        This project was created by
+        <a
+          href="https://www.shecodes.io/graduates/120875-millicent-kapinga"
+          target="_blank"
+        >
+          Millicent Kapinga
+        </a>{" "}
+        and is{" "}
+        <a href="https://github.com/Millykapy/weather.js " target="_blank">
+          Open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
