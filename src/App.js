e2d9001fb996a3e687d5153296b7a2b82aded9c5
Millykapy <168423257+Millykapy@users.noreply.github.com>
2024-07-23 14:43:33 +0200

    added some html and made changes on css

--- src/App.js
+++ src/App.js
@@ -6,18 +6,36 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1> Spooky Weather</h1>
+        <h3> Spooky Weather</h3>
         <Weather defaultCity="Johannesburg" />
         <footer>
           This project was created by{" "}
-          <a href="https://www.shecodes.io/graduates/120875-millicent-kapinga">
+          <a
+            href="https://www.shecodes.io/graduates/120875-millicent-kapinga"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             Millicent Kapinga
           </a>{" "}
-          and is{" "}
-          <a href="https://github.com/Millykapy/weather.js ">
-            Open-sourced on Github
+          and is{"  "}
+          <a
+            href="https://github.com/Millykapy/weather.js "
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Open-sourced on Github{"  "}
+          </a>{" "}
+          {"  "}
+          <a
+            href="https://snazzy-entremet-e4a99e.netlify.app/ "
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {"  "}
+            hosted on Netlify
           </a>
         </footer>
+        <p>Happy Halloween!</p>
       </div>
     </div>
   );
